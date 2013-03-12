ed6198615b3cf0e7837c16494e92c09b684b7235
Peter Ballok <pballok@gmail.com>
2013-03-12 18:37:30 +0100

    Fixed build errors

--- src/com/ge/innovation/accreadout/DisplayMessageActivity.java
+++ src/com/ge/innovation/accreadout/DisplayMessageActivity.java
@@ -1,6 +1,14 @@
+package com.ge.innovation.accreadout;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.content.Intent;
+import android.widget.TextView;
+import android.support.v4.app.NavUtils;
+
 public class DisplayMessageActivity extends Activity {
 
-  @SuppressLint("NewApi")
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -14,11 +22,8 @@ public class DisplayMessageActivity extends Activity {
 
     setContentView(textView);
 
-    // Make sure we're running on Honeycomb or higher to use ActionBar APIs
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-      // Show the Up button in the action bar.
-      getActionBar().setDisplayHomeAsUpEnabled(true);
-    }
+    // Show the Up button in the action bar.
+    getActionBar().setDisplayHomeAsUpEnabled(true);
   }
 
   @Override
