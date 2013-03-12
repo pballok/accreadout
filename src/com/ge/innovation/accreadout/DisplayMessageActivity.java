9d78b1d87db96e88cab47f23c7a9d93284a83e64
Peter Ballok <pballok@gmail.com>
2013-03-12 18:16:08 +0100

    Added a second Activity

---
+++ src/com/ge/innovation/accreadout/DisplayMessageActivity.java
@@ -0,0 +1,35 @@
+public class DisplayMessageActivity extends Activity {
+
+  @SuppressLint("NewApi")
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    
+    Intent intent = getIntent();
+    String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+    TextView textView = new TextView(this);
+    textView.setTextSize(40);
+    textView.setText(message);
+
+    setContentView(textView);
+
+    // Make sure we're running on Honeycomb or higher to use ActionBar APIs
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+      // Show the Up button in the action bar.
+      getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    switch (item.getItemId()) {
+      case android.R.id.home:
+        NavUtils.navigateUpFromSameTask(this);
+        return true;
+      }
+    return super.onOptionsItemSelected(item);
+  }
+}
+
+
--- src/com/ge/innovation/accreadout/MainActivity.java
+++ src/com/ge/innovation/accreadout/MainActivity.java
@@ -2,14 +2,27 @@ package com.ge.innovation.accreadout;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.widget.EditText;
 
 public class MainActivity extends Activity
 {
+    public final static String EXTRA_MESSAGE = "com.ge.innovation.accreadout.MESSAGE";
+    
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+    
+    public void sendMessage(View view) {
+      Intent intent = new Intent(this, DisplayMessageActivity.class);
+      EditText editText = (EditText) findViewById(R.id.edit_message);
+      String message = editText.getText().toString();
+      intent.putExtra(EXTRA_MESSAGE, message);
+      startActivity(intent);
     }
 }
